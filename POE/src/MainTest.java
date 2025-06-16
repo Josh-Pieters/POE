49a2e145dbcdf29e698ddd2ecbd85496bbda36ff
Josh-Pieters <ST10466207@rcconnect.edu.za>
2025-06-16 11:18:34 +0000

    Add files via upload

--- POE/src/MainTest.java
+++ POE/src/MainTest.java
@@ -31,4 +31,107 @@ class MainTest {
         }
     }
 
+    String[] messages = {
+            "Did you get the cake?",
+            "It is dinner time!",
+            "Where are you? You are late! I have asked you to be on time.",
+            "Ok, I am leaving without you."
+    };
+
+    String[] ids = {
+            "0001", "0002", "0838884567", "0004"
+    };
+
+    String[] recipients = {
+            "+27838884567", "+27838884567", "+27838884567", "+27111111111"
+    };
+
+    String[] hashes = {
+            "HASH1", "HASH2", "HASH3", "HASH4"
+    };
+
+    // --- Array-based tests ---
+
+    @org.junit.jupiter.api.Test
+    void testMessagesArrayContainsExpectedData() {
+        boolean found1 = false, found2 = false;
+
+        for (String msg : messages) {
+            if (msg.equals("Did you get the cake?")) found1 = true;
+            if (msg.equals("It is dinner time!")) found2 = true;
+        }
+
+        assertTrue(found1);
+        assertTrue(found2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLongestMessage() {
+        String longest = "";
+        for (String msg : messages) {
+            if (msg.length() > longest.length()) {
+                longest = msg;
+            }
+        }
+
+        assertEquals("Where are you? You are late! I have asked you to be on time.", longest);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSearchMessageByID() {
+        String result = "";
+        for (int i = 0; i < ids.length; i++) {
+            if (ids[i].equals("0838884567")) {
+                result = messages[i];
+                break;
+            }
+        }
+
+        assertEquals("Where are you? You are late! I have asked you to be on time.", result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSearchByRecipient() {
+        String[] expected = {
+                "Where are you? You are late! I have asked you to be on time.",
+                "Ok, I am leaving without you."
+        };
+
+        String[] found = new String[2];
+        int index = 0;
+
+        for (int i = 0; i < recipients.length; i++) {
+            if (recipients[i].equals("+27838884567")) {
+                found[index++] = messages[i];
+            }
+        }
+
+        assertEquals(expected[0], found[0]);
+        assertEquals(expected[1], found[1]);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDeleteMessageByHash() {
+        String hashToDelete = "HASH2";
+        String deletedMessage = "";
+
+        for (int i = 0; i < hashes.length; i++) {
+            if (hashes[i].equals(hashToDelete)) {
+                deletedMessage = messages[i];
+                break;
+            }
+        }
+
+        assertEquals("Where are you? You are late! I have asked you to be on time.", deletedMessage);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDisplayReport() {
+        for (int i = 0; i < messages.length; i++) {
+            assertNotNull(hashes[i]);
+            assertNotNull(recipients[i]);
+            assertNotNull(messages[i]);
+        }
+    }
+
 }
